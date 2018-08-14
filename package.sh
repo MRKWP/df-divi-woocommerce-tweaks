9c9e739439b3b283f5795e95d3321dca2c5a06a2
Ketan Shah <ketan.shah@gmail.com>
2018-08-14 12:27:26 +0530

    fixed button styles where divi colour scheme was selected

--- package.sh
+++ package.sh
@@ -16,6 +16,6 @@ zip -r9 $plugin_basename-$version.zip $plugin_basename -x *.git* -x *.sh -x *.js
 rm -rf /tmp/$plugin_basename;
 
 #upload to s3.
-rclone mkdir df-s3:diviframework/$plugin_basename;
-rclone copy /tmp/$plugin_basename-$version.zip df-s3:diviframework/$plugin_basename;
-echo "https://s3-ap-southeast-2.amazonaws.com/diviframework/$plugin_basename/$plugin_basename-$version.zip"
+# rclone mkdir df-s3:diviframework/$plugin_basename;
+# rclone copy /tmp/$plugin_basename-$version.zip df-s3:diviframework/$plugin_basename;
+# echo "https://s3-ap-southeast-2.amazonaws.com/diviframework/$plugin_basename/$plugin_basename-$version.zip"
