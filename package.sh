4c78a5aca1d5abba8d01aa1807f1e417b5a01aaa
Ketan Shah <ketan.shah@gmail.com>
2018-08-15 15:32:46 +0530

    when divi color scheme is selected, divi tweaks styles on cart and checkout not applied. fixed.

--- package.sh
+++ package.sh
@@ -12,10 +12,11 @@ cd -;
 cd /tmp;
 rm /tmp/$plugin_basename/sftp-config.json;
 rm -rf /tmp/$plugin_basename/vendor/diviframework/update-checker/.git;
+rm -rf /tmp/$plugin_basename/docs;
 zip -r9 $plugin_basename-$version.zip $plugin_basename -x *.git* -x *.sh -x *.json -x *.xml -x *.dist -x *.lock -x *tests* -x *bin* -x *Gruntfile.js* -x *.gitignore* -x *.distignore* -x *.editorconf*;
 rm -rf /tmp/$plugin_basename;
 
 #upload to s3.
-# rclone mkdir df-s3:diviframework/$plugin_basename;
-# rclone copy /tmp/$plugin_basename-$version.zip df-s3:diviframework/$plugin_basename;
-# echo "https://s3-ap-southeast-2.amazonaws.com/diviframework/$plugin_basename/$plugin_basename-$version.zip"
+rclone mkdir df-s3:diviframework/$plugin_basename;
+rclone copy /tmp/$plugin_basename-$version.zip df-s3:diviframework/$plugin_basename;
+echo "https://s3-ap-southeast-2.amazonaws.com/diviframework/$plugin_basename/$plugin_basename-$version.zip"
